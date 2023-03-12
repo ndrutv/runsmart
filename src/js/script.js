a2fb2b3c764bb743cf6a33085d17dff0e9a57453
Andrey Fedorov <info@ndru.tv>
2023-03-12 13:49:41 +0400

    fix: fix some problems with catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+// Slider
 const slider = tns({
     container: '.carousel__inner',
     controls: false,
@@ -10,8 +11,6 @@ const slider = tns({
     ]
 });
 
-
-// Slider
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 
@@ -23,57 +22,66 @@ next.addEventListener('click', () => {
     slider.goTo('next');
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Catalog
+    // -- Tabs
+    const tabs = document.querySelectorAll('.catalog__tab');
+    const catalog = document.querySelectorAll('.catalog__content');
 
-// Catalog
-// -- Tabs
-const tabs = document.querySelectorAll('.catalog__tab');
-const catalog = document.querySelectorAll('.catalog__content');
+    const hideCatalog = () => {
+        tabs.forEach(tab => {
+            tab.classList.remove('catalog__tab_active');
+        });
 
-const hideCatalog = () => {
-    tabs.forEach(tab => {
-        tab.classList.remove('catalog__tab_active');
+        catalog.forEach(item => {
+            item.classList.remove('catalog__content_active');
+        });
+    }
+
+    const showCatalog = (i = 0) => {
+        catalog[i].classList.add('catalog__content_active');
+        tabs[i].classList.add('catalog__tab_active');
+    }
+
+    showCatalog();
+
+    tabs.forEach((tab, i) => {
+        tab.addEventListener('click', (e) => {
+            e.preventDefault();
+            hideCatalog();
+            showCatalog(i);
+        })
     });
 
-    catalog.forEach(item => {
-        item.classList.remove('catalog__content_active');
-    });
-}
+    
+    // Catalog
+    // -- Items
+    const content = document.querySelectorAll('.catalog-item__content');
+    const list = document.querySelectorAll('.catalog-item__list');
+    const links = document.querySelectorAll('.catalog-item__link');
+    const backs = document.querySelectorAll('.catalog-item__back');
 
-const showCatalog = (i = 0) => {
-    catalog[i].classList.add('catalog__content_active');
-    tabs[i].classList.add('catalog__tab_active');
-}
-
-tabs.forEach((tab, i) => {
-    tab.addEventListener('click', (e) => {
-        e.preventDefault();
-        hideCatalog();
-        showCatalog(i);
+    content.forEach((item) => {
+        item.classList.add('catalog-item__content_active');
     })
-});
 
+    const toggleContent = (i) => {
+        content[i].classList.toggle('catalog-item__content_active');
+        list[i].classList.toggle('catalog-item__list_active');
+    }
 
-// -- Items
-const content = document.querySelectorAll('.catalog-item__content');
-const list = document.querySelectorAll('.catalog-item__list');
-const links = document.querySelectorAll('.catalog-item__link');
-const backs = document.querySelectorAll('.catalog-item__back');
-
-const toggleContent = (i) => {
-    content[i].classList.toggle('catalog-item__content_active');
-    list[i].classList.toggle('catalog-item__list_active');
-}
-
-links.forEach((link, i) => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        toggleContent(i);
+    links.forEach((link, i) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            toggleContent(i);
+        })
     })
-})
 
-backs.forEach((back, i) => {
-    back.addEventListener('click', (e) => {
-        e.preventDefault();
-        toggleContent(i);
+    backs.forEach((back, i) => {
+        back.addEventListener('click', (e) => {
+            e.preventDefault();
+            toggleContent(i);
+        })
     })
 })
