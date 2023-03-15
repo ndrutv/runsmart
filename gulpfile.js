0d6cff8aa10c92d1fc95571341ac96af8b803fc0
Andrey Fedorov <info@ndru.tv>
2023-03-15 12:52:28 +0400

    refactor: update gulpfile, final build in dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,14 @@ const sass        = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const htmlmin = require('gulp-htmlmin');
 
 // live server
 gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "dist"
         }
     });
 
@@ -24,14 +25,37 @@ gulp.task('styles', function() {
         .pipe(rename({suffix: '.min', prefix: ''}))
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
 // watch
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+});
+
+gulp.task('html', function() {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"))
 })
 
+gulp.task('images', function () {
+    return gulp.src("src/assets/images/**/*")
+        // .pipe(imagemin())    
+        .pipe(gulp.dest("dist/assets/images"));
+});
+
+gulp.task('icons', function () {
+    return gulp.src("src/assets/icons/**/*")
+        .pipe(gulp.dest("dist/assets/icons"));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
+
 // combine
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'html', 'images', 'icons', 'scripts'));
